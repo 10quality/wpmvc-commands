fdf5b3605038b75df548dbc135697ce809ad4057
Alejandro Mostajo <amostajo@gmail.com>
2020-01-15 23:40:41 -0600

    Unit testing improved.
    Testing environment `Main.php` is now removed from project as it gets wipedout and generated on every test execution.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,9 @@ require_once __DIR__.'/framework/class.WpmvcAyucoTestCase.php';
 define('FRAMEWORK_PATH', __DIR__);
 define('ENV_PATH', __DIR__.'/environments');
 define('WPMVC_AYUCO', FRAMEWORK_PATH.'/environment/ayuco');
+define('TESTING_PATH', __DIR__.'/environment');
+// Delete existing main
+$main = TESTING_PATH.'/app/Main.php';
+if (file_exists($main))
+    unlink($main);
+file_put_contents($main, '<?php namespace MyApp; use WPMVC\Bridge; class Main extends Bridge { public function init(){ } public function on_admin() { } }');
