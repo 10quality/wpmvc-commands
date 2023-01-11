d3ea994e5bfad850350d855e818837af215004f6
Ale Mostajo <amostajo@gmail.com>
2023-01-11 12:46:50 -0600

    Fixes 3rd party dependencies initial load out

--- templates/testbootstrap.php
+++ templates/testbootstrap.php
@@ -38,6 +38,15 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_wpmvc_project() {
+    // Load WordPress MVC project
+    require dirname( dirname( __FILE__ ) ) . '/{0}';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_wpmvc_project' );
+
+/**
+ * Manually load 3rd party plugins that are dependencies.
+ */
+function _manually_load_external_plugins() {
     // ----------------------------
     //
     // Load plugin dependencies here, example below:
@@ -45,11 +54,9 @@ function _manually_load_wpmvc_project() {
     // require_once WP_CONTENT_DIR . '/plugins/woocommerce/woocommerce.php';
     //
     // ----------------------------
-    //
-    // Load WordPress MVC project
-    require dirname( dirname( __FILE__ ) ) . '/{0}';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_wpmvc_project' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+_manually_load_external_plugins();
